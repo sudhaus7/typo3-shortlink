8cd7569c749ca35013b28ec8ea02d3fdd0d34371
Frank Berger <fberger@sudhaus7.de>
2023-03-14 13:03:46 +0100

    push version for 11

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@ $EM_CONF[$_EXTKEY] = [
     'title' => '(Sudhaus7) URL/Link Shortener',
     'description' => 'A URL/Link Shortener for the TYPO3 frontend',
     'category' => 'plugin',
-    'version' => '2.0.0',
+    'version' => '3.0.0',
     'state' => 'stable',
     'uploadfolder' => 0,
     'clearcacheonload' => 0,
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'Sudhaus7, ein Label der B-Factor GmbH',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-10.4.99'
+            'typo3' => '11.5.0-11.5.99'
         ],
         'conflicts' => [
         ],
